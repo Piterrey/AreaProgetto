5102bf21937d2771d009d43e3a030aa2398e195a
Piterrey <sapartangiu@libero.it>
2014-05-28 11:46:04 +0200

    Update mpu6050axisReader.ino

--- mpu6050axisReader.ino
+++ mpu6050axisReader.ino
@@ -1,12 +1,3 @@
-#include <PID_v1.h>
-
-#include <DateTime.h>
-/*------------------------------------------Importante------------------------------*/
-  /*       aggiunta alla libreria DateTime.cpp: #include <Arduino.h>   */
-/*----------------------------------------------------------------------------------*/
-  
-#include <DateTimeStrings.h>
-
 #include <I2Cdev.h>
 
 #include <MPU6050.h>
@@ -15,47 +6,9 @@
 
 #include "Wire.h"
 
-//#include <MPU6050.h>
 
-//#include <I2Cdev.h>
-
-// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
-
-
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-
-
-
-
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for InvenSense evaluation board)
-// AD0 high = 0x69
-
-//MPU6050 accelgyro(0x69); // <-- use for AD0 high
-
-
-
-
-// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
-// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
-// not so easy to parse, and slow(er) over UART.
 #define OUTPUT_READABLE_ACCELGYRO
-
-// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
-// binary, one right after the other. This is very fast (as fast as possible
-// without compression or data loss), and easy to parse, but impossible to read
-// for a human.
-//#define OUTPUT_BINARY_ACCELGYRO
-
-
-/*#define LED_PIN_AX 13
-#define LED_PIN_LAX 12
-#define LED_PIN_AY 11
-#define LED_PIN_LAY 10*/
-
+#define KILLZONE 30
 #define DEADZONE 5
 /*-------------------------------------------------Variabili--------------------------*/
     MPU6050 accelgyro;
@@ -64,14 +17,11 @@
     int i=0;
     double angoloGx=0;
     double angoloAx=0;
-    time_t time=0;
-    unsigned long nowTime=0;
-    unsigned long oldTime=0;
-    double fiAngx=0;
-    double kP, kI, kD;
-    PID myPID;
-    double Setpoint = 2, Input, Output;
-    double samplingTime = 1/37;
+    double angoloFx=0;
+    double kP=8, kI=0, kD=0, P, I, D, T_PID;
+    double setPoint, error, prevError;
+    double sampleTime;
+    unsigned long time, prevTime;
 /*------------------------------------------------------------------------------------*/
 
 void setup() {
@@ -99,19 +49,10 @@ void setup() {
       pinMode(i,OUTPUT);
     }
     i=0;
-    // use the code below to change accel/gyro offset values
-    /*
-    Serial.println("Updating internal sensor offsets...");
-    // -76        -2359        1688        0        0        0
-    accelgyro.setXGyroOffset(220);
-    accelgyro.setYGyroOffset(76);
-    accelgyro.setZGyroOffset(-85);
     
-    */
-
     // configure Arduino LED for
     Serial.print("       ax     |gx     |sec    |Filtered angle  |\n");
-    /*-------------------------------------------------------------------------- imposto l'angolo inziale per il giroscopio -----------------*/
+/*------------------------------------------------------------------------- imposto l'angolo inziale per il giroscopio -------------------*/
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
     
     angoloAx=atan((ax)/(sqrt(pow(ay,2)+pow(az,2))));
@@ -120,50 +61,117 @@ void setup() {
     
     angoloGx = angoloAx;
     
-    fiAngx= angoloAx;
+    angoloFx = angoloAx;
+    
+    setPoint = 0;
     
     Serial.print(angoloAx); Serial.print("\t");
       
     Serial.print(angoloGx); Serial.print("\t");
-    /*---------------------------------------------------------------------- setto l'ora a zero --------------------------------------------*/
-    
-    DateTime.sync(time);
-    
-    myPID(&Input, &Output, &Setpoint,kP,kI,kD,DIRECT);
-    myPID.SetMode(AUTOMATIC);
-    myPID.SetOutputLimits(0,255);
-    myPID.SetSamplingTime(samplingTime);
     
 }
 
-void loop() {
-    // read raw accel/gyro measurements from device
+/*------------------------------------------------------------------------- funzione che elabora la tensione e restituisce gli angoli ----*/
+void GetTilt()
+{
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    
+    angoloAx = atan((ax)/(sqrt(pow(ay,2)+pow(az,2))));
+    
+    angoloAx = angoloAx*(180/M_PI);
+    
+    angoloGx = angoloFx + ((gx/131) * (sampleTime/1000));
+    
+}
+/*------------------------------------------------------------------------- elabora l'errore applicando il controllo PID -----------------*/
+void ComputePID()
+{
+    error = angoloFx - setPoint;
+    
+    if(error > 0) 
+    {
+      kP = 7.6;
+    }
+    else
+    {
+      kP = 15;
+    }
+    
+    P = error * kP;
+    
+    I = I + (error * sampleTime);
+    I = I * kI;
+       
+    D = (error - prevError) * kD;
 
-        // 
-        /*------------------------------------------------------------------- lettura ed elaborazione dati accelerometro -------------------*/
-        angoloAx=atan((ax)/(sqrt(pow(ay,2)+pow(az,2))));
+    prevError = error;    
+    
+    T_PID = abs(P + I + D);
+    
+    if(T_PID > 255)
+    {
+        T_PID = 255;    
+    }
+    
+}
+/*------------------------------------------------------------------------- aplica il filtro complementare sull'angolo misuarto ----------*/
+void ComplementaryFilter()
+{
+    angoloFx = (0.75)*(angoloGx)+(0.25)*(angoloAx);
+}
+/*------------------------------------------------------------------------- ottiene il tempo di campionamento ----------------------------*/
+void GetSampleTime()
+{
+  time = millis();
+  
+  sampleTime = time - prevTime;
+  
+  prevTime = time;
+}
+/*------------------------------------------------------------------------- funzione per il muovimento dei motori ------------------------*/
+void DriveMotor()
+{
+    if((angoloFx < setPoint + DEADZONE) && (angoloFx > setPoint - DEADZONE))
+    {
+        digitalWrite(9, HIGH);
+        digitalWrite(8, HIGH);
+    }
+    else
+    {
+        digitalWrite(9, LOW);
+        digitalWrite(8, LOW);
         
-        /*------------------------------------------------------------------- conversione da radianti a gradi ------------------------------*/
-        angoloAx=angoloAx*(180/M_PI);
+        if(angoloFx > 0) 
+        {
+           digitalWrite(13, HIGH);
+           digitalWrite(12, LOW);
+        }
+        else
+        {
+           digitalWrite(13, LOW);
+           digitalWrite(12, HIGH);
+        }
         
-        /*------------------------------------------------------------------- salvo i secondi correnti in una variabile---------------------*/
-        nowTime = DateTime.now(); 
+        if(abs(angoloFx) <= KILLZONE)
+        {
+            analogWrite(11, T_PID);
+            analogWrite(3, T_PID);
+        }
+        else
+        {
+            analogWrite(11, 0);
+            analogWrite(3, 0);
+        }
+    }
+}
+
+void loop() {
+        GetSampleTime();
+        GetTilt();
+        ComplementaryFilter();
+        ComputePID();
+        DriveMotor();
         
-        /*------------------------------------------------------------------- elaborazione dati giroscopio ---------------------------------*/
-        
-          angoloGx = fiAngx+(gx/(131)*samplingTime);
-          
-          oldTime=nowTime;
-          
-          /*------- risetto i secondi a 0 per non andare in overflow --------*/
-          if(nowTime>=60)
-          {
-            DateTime.sync(time);
-          }
-        /*------------------------------------------------------------------- filtro complementare -----------------------------------------*/
-        
-        fiAngx = (0.75)*(angoloGx)+(0.25)*(angoloAx);
         
         /*------------------------------------------------------------------- print dei risultati ------------------------------------------*/
         Serial.print("a/g:\t");
@@ -172,30 +180,8 @@ void loop() {
         
         Serial.print(angoloGx); Serial.print("\t");
         
-        Serial.print(nowTime); Serial.print("\t");
+        Serial.print(angoloFx); Serial.print("\t");
         
-        Serial.print(fiAngx); Serial.print("\n");
-        
-        /*------------------------------------------------------------------ Controllo PID -------------------------------------------------*/
-        
-        if(fiAngx>setPoint+DEADZONE | fiAngx<setPoint-DEADZONE)
-        {
-          myPID.Compute();
-          digitalWrite(9,LOW);
-          digitalWrite(8,LOW);
-        }
-        else
-        {
-          digitalWrite(9,HIGH);
-          digitalWrite(8,HIGH);
-        }
-        /*------------------------------------------------------------------ if per circuito di prova --------------------------------------*/
-        //13 dir A
-        //12 dir B
-        //11 pwm B
-        //9  brake A
-        //8  brake B
-        //3  pwm A
-    
+        Serial.print(T_PID); Serial.print("\n");
 
 }
